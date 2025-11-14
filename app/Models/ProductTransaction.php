0686b56acb312c5e0b8c7d6ff23ebabcc7c2924b
Galuh <gitgaluh@gmail.com>
2025-11-14 21:10:08 +0700

    feat: add initial ecommerce models (category, brand, shoe, shoe_photo, shoe_size, promo_code, product_transaction)

--- app/Models/ProductTransaction.php
+++ app/Models/ProductTransaction.php
@@ -3,8 +3,51 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'booking_trx_id',
+        'city',
+        'post_code',
+        'proof',
+        'shoe_size',
+        'address',
+        'quantity',
+        'sub_total_amount',
+        'grand_total_amount',
+        'discount_amount',
+        'is_paid',
+        'shoe_id',
+        'promo_code_id'
+    ];
+
+    public static function generateUniqueTrxId()
+    {
+        $prefix = 'TVS';
+
+        do {
+            $randomString = $prefix . mt_rand(1000, 99990);
+        } while (self::where('booking_trx_id', $randomString)->exist());
+
+        return $randomString;
+    }
+
+    public function shoe(): BelongsTo
+    {
+        return $this->belongsTo(Shoe::class, 'shoe_id');
+    }
+
+    public function promoCode(): BelongsTo
+    {
+        return $this->belongsTo(PromoCode::class, 'promo_code_id');
+    }
 }
