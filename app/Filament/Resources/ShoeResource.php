c598ebb5b60c9bcb42e937e6187b04a6cfd0f90e
Galuh <gitgaluh@gmail.com>
2025-11-17 18:55:00 +0700

    feat(filament): update sidebar logo

--- app/Filament/Resources/ShoeResource.php
+++ app/Filament/Resources/ShoeResource.php
@@ -17,7 +17,7 @@ class ShoeResource extends Resource
 {
     protected static ?string $model = Shoe::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-cube';
 
     public static function form(Form $form): Form
     {
